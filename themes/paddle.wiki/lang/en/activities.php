5bfc1ed55cf7a65fc908c69b3a0014c71cb6c346
Simon Maxwell-Stewart <smaxwellstewart@gmail.com>
2021-06-24 13:34:00 -0400

    added custom paddle.wiki theme

---
+++ themes/paddle.wiki/lang/en/activities.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Activity text strings.
+ * Is used for all the text within activity logs & notifications.
+ */
+return [
+
+    // Pages
+    'page_create'                 => 'created page',
+    'page_create_notification'    => 'Page Successfully Created',
+    'page_update'                 => 'updated page',
+    'page_update_notification'    => 'Page Successfully Updated',
+    'page_delete'                 => 'deleted page',
+    'page_delete_notification'    => 'Page Successfully Deleted',
+    'page_restore'                => 'restored page',
+    'page_restore_notification'   => 'Page Successfully Restored',
+    'page_move'                   => 'moved page',
+
+    // Chapters
+    'chapter_create'              => 'created section',
+    'chapter_create_notification' => 'Section Successfully Created',
+    'chapter_update'              => 'updated section',
+    'chapter_update_notification' => 'Section Successfully Updated',
+    'chapter_delete'              => 'deleted section',
+    'chapter_delete_notification' => 'Section Successfully Deleted',
+    'chapter_move'                => 'moved section',
+
+    // Books
+    'book_create'                 => 'created route',
+    'book_create_notification'    => 'Route Successfully Created',
+    'book_update'                 => 'updated route',
+    'book_update_notification'    => 'Route Successfully Updated',
+    'book_delete'                 => 'deleted route',
+    'book_delete_notification'    => 'Route Successfully Deleted',
+    'book_sort'                   => 'sorted route',
+    'book_sort_notification'      => 'Route Successfully Re-sorted',
+
+    // Bookshelves
+    'bookshelf_create'            => 'created Region',
+    'bookshelf_create_notification'    => 'Region Successfully Created',
+    'bookshelf_update'                 => 'updated region',
+    'bookshelf_update_notification'    => 'Region Successfully Updated',
+    'bookshelf_delete'                 => 'deleted region',
+    'bookshelf_delete_notification'    => 'Region Successfully Deleted',
+
+    // Favourites
+    'favourite_add_notification' => '":name" has been added to your favourites',
+    'favourite_remove_notification' => '":name" has been removed from your favourites',
+
+    // Other
+    'commented_on'                => 'commented on',
+    'permissions_update'          => 'updated permissions',
+];
